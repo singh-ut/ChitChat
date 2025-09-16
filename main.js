ccbd3c4a960326710728b1ebdd477a9efe956a7e
Utkarsh Singh Tomar <devutkarsh.codes@gmail.com>
2025-09-16 18:56:26 +0530

    refactored

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 /* ### SELECTING ALL ELEMENTS USING 'querySelector' ### */
 const send_btn = document.querySelector("#send-btn");
 const user_input = document.querySelector("#message-input");
+const chat_window = document.querySelector("#chat-window");
 
 // ### MESSAGE SENDING FUNCTIONALITY ###
 const sendMsg = () => {
   const user_message = user_input.value.trim();
 
-  console.log(user_message);
+  if (user_message !== "") {
+    const message_element = document.createElement("div");
+    message_element.classList.add("message");
+    message_element.innerText = user_message;
+    chat_window.appendChild(message_element);
+    chat_window.scrollTop = chat_window.scrollHeight;
+  }
 
   user_input.value = "";
 };
